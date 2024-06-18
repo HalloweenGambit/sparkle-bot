a3249702d56cc729e4fbf19d05c874dd5655e940
J <0xjariel@protonmail.com>
2024-06-18 02:29:59 -0600

    before typsecript migration

--- src/bot/eventListeners/questionListener.js
+++ src/bot/eventListeners/questionListener.js
@@ -1,5 +1,5 @@
 // src/bot/eventListeners/questionListener.js
-import { handleMessageCreate } from "../commands/text/question.js";
+import { handleMessageCreate } from "../commands/question.js";
 
 export default function questionListener(discordClient, dbClient, gptClient) {
   discordClient.on("messageCreate", (message) =>
