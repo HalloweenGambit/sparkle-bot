12dc8cdd27a5d3c661bc4558c6e659b1b2b81140
J <0xjariel@protonmail.com>
2024-06-24 01:48:43 -0600

    Resolved merge conflict

--- src/bot/eventListeners/messageCreate.ts
+++ src/bot/eventListeners/messageCreate.ts
@@ -1,22 +1,14 @@
 import { handleQuestion } from "../commands/text/handleQuestion";
-<<<<<<< HEAD
-=======
-
->>>>>>> f7f077b
 const regex = /^hey doc/i;
 
 export default (client) => {
-  client.on("messageCreate", async (message, dbClient, gptClient) => {
+  client.on("messageCreate", async (message) => {
     if (regex.test(message.content)) {
       console.log("I was summoned");
       console.log(message);
       message.reply("hello");
     }
 
-<<<<<<< HEAD
-    handleQuestion(message, dbClient, gptClient);
-=======
     handleQuestion(message);
->>>>>>> f7f077b
   });
 };
