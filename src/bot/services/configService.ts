602cb9d243b5f7ab71720174583617c6811914f6
J <0xjariel@protonmail.com>
2024-07-17 19:20:44 -0600

    adding guild configuration services

--- src/bot/services/configService.ts
+++ src/bot/services/configService.ts
@@ -1,17 +1,17 @@
 import { Snowflake } from 'discord.js'
 
-export const saveConfig = async (discordId: Snowflake) => {
-  console.log('Creating a new configuration for Discord ID:', discordId)
+export const createAndSaveConfig = async (guildId: Snowflake) => {
+  console.log('Creating a new configuration for Discord ID:', guildId)
 
   return true
 }
 
-export const loadConfig = async (discordId: Snowflake) => {
-  console.log('Loading configuration for Discord ID:', discordId)
+export const loadConfig = async (guildId: Snowflake) => {
+  console.log('Loading configuration for Discord ID:', guildId)
 
   return true
 }
 
-export const deleteConfig = async (discordId: Snowflake) => {}
+export const deleteConfig = async (guildId: Snowflake) => {}
 
-export const updateConfig = async (discordId: Snowflake) => {}
+export const updateConfig = async (guildId: Snowflake) => {}
